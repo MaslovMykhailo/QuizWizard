bd9d174484a37ddc0162c6a9c103798ab4c72250
Mykhailo Maslov <maslov.mikhail2000@gmail.com>
2020-05-03 21:06:35 +0300

    Init auth repo

---
+++ auth/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+          'newlines-between': 'always'
+      }
+    ],
+    'comma-dangle': [
+      'error', {
+        'arrays': 'never',
+        'objects': 'never',
+        'imports': 'never',
+        'exports': 'never',
+        'functions': 'never'
+      }
+    ],
+    'semi': ['error', 'never']
+  }
+}
