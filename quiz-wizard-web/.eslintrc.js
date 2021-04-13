bed334a8e191b616ad149d5a6508553f45cdc504
Mykhailo Maslov <maslov.mikhail2000@gmail.com>
2021-04-13 22:29:05 +0300

    Init redux module

--- quiz-wizard-web/.eslintrc.js
+++ quiz-wizard-web/.eslintrc.js
@@ -4,11 +4,39 @@ module.exports = {
     'react-app',
     'react-app/jest',
     'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'prettier',
     '../.eslintrc.js'
   ],
   plugins: ['jsx-a11y'],
   rules: {
     'quotes': ['error', 'single'],
-    'react/jsx-indent' : ['error', 2]
+    'react/jsx-indent' : ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-children-prop': 'off',
+    'react/jsx-first-prop-new-line': 'error',
+    'react/jsx-max-props-per-line': 'error',
+    'react/self-closing-comp': 'error',
+    'react/jsx-closing-bracket-location': 'error',
+    'react/jsx-wrap-multilines': [
+        'error',
+        {
+            declaration: 'parens-new-line',
+            assignment: 'parens-new-line',
+            return: 'parens-new-line',
+            arrow: 'parens-new-line',
+            condition: 'parens-new-line',
+            logical: 'parens-new-line',
+            prop: 'ignore'
+        }
+    ],
+    'react/jsx-one-expression-per-line': [
+      'error',
+      {
+        allow: 'single-child'
+      }
+    ]
   }
 }
--- quiz-wizard-web/src/app.tsx
+++ quiz-wizard-web/src/app.tsx
@@ -1,5 +1,50 @@
 import {FC} from 'react'
+import {Provider, useDispatch, useSelector} from 'react-redux'
+import {createQuizWizardStore, decrement, increment} from 'quiz-wizard-redux' 
 
 export const App: FC = () => {
-  return <h2 children="QuizWizard" />
+  return (
+    <>
+      <h2 children="QuizWizard" />
+      <Store>
+        <Counter />
+      </Store>
+    </>
+  )
+}
+
+const store = createQuizWizardStore()
+
+const Store: FC = (props) => {
+  return (
+    <Provider
+      store={store}
+      {...props}
+    />
+  )
+}
+
+const Counter: FC = () => {
+  const value = useSelector(state => (state as {value: number}).value)
+
+  const dispatch = useDispatch()
+
+  const onIncrement = () => dispatch(increment())
+  const onDecrement = () => dispatch(decrement())
+
+  return (
+    <div>
+      <button
+        onClick={onIncrement}
+        children="Increment"
+      />
+      <br />
+      {value}
+      <br />
+      <button
+        onClick={onDecrement}
+        children="Decrement"
+      />
+    </div>
+  )
 }
