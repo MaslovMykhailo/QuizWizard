84bcecf1cfba0a8417079237d0a5b99f712e22f4
Mykhailo Maslov <maslov.mikhail2000@gmail.com>
2021-04-21 19:06:02 +0300

    Adjust palette

--- quiz-wizard-web/src/providers/theme.tsx
+++ quiz-wizard-web/src/providers/theme.tsx
@@ -1,5 +1,3 @@
-// import {createMuiTheme, , Switch, Theme, ThemeProvider as MiuThemProvider, useTheme} from '@material-ui/core'
-
 import {useSelector} from 'react-redux'
 import {ThemeType} from 'quiz-wizard-schema'
 import {selectThemeType} from 'quiz-wizard-redux'
@@ -8,13 +6,39 @@ import {
   ThemeProviderProps as MuiThemeProviderProps,
   createMuiTheme
 } from '@material-ui/core/styles'
+import type {PaletteOptions} from '@material-ui/core/styles/createPalette'
+import deepPurple from '@material-ui/core/colors/deepPurple'
+import lightGreen from '@material-ui/core/colors/lightGreen'
+import lightBlue from '@material-ui/core/colors/lightBlue'
+import orange from '@material-ui/core/colors/orange'
+import pink from '@material-ui/core/colors/pink'
+import red from '@material-ui/core/colors/red'
+
+const palettes: Record<ThemeType, PaletteOptions> = {
+  light: {
+    type: 'light',
+    primary: deepPurple,
+    secondary: pink,
+    error: red,
+    warning: orange,
+    info: lightBlue,
+    success: lightGreen
+  },
+  dark: {
+    type: 'dark',
+    primary: pink,
+    secondary: deepPurple,
+    error: red,
+    warning: orange,
+    info: lightBlue,
+    success: lightGreen
+  }
+}
 
 const createTheme = (
   type: ThemeType
 ) => createMuiTheme({
-  palette: {
-    type
-  }
+  palette: palettes[type]
 })
 
 export type ThemeProviderProps = Omit<MuiThemeProviderProps, 'theme'>
