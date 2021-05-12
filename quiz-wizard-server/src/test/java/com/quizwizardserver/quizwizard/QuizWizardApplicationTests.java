b52c58a0a87d32c9cd3a68d01be4262b16509315
Mykhailo Maslov <maslov.mikhail2000@gmail.com>
2021-05-12 18:56:12 +0300

    Init QuizWizard server

---
+++ quiz-wizard-server/src/test/java/com/quizwizardserver/quizwizard/QuizWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizwizardserver.quizwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
